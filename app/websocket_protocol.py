3311be673bee7e7d88d54728ffc445db782ed611
Ernesto Ruge <mail@ernestoruge.de>
2021-06-17 13:39:03 +0200

    full copyright

--- app/websocket_protocol.py
+++ app/websocket_protocol.py
@@ -3,7 +3,7 @@ Websockets protocol
 
 MIT License
 
-Copyright (c) 2019 Danielle Madeley
+Copyright (c) 2019 Danielle Madeley, 2021 binary butterfly GmbH
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
