71e5d7529283a3effb12294f163dc62ca51b3bdc
Ernesto Ruge <mail@ernestoruge.de>
2021-06-23 08:47:32 +0200

    separate open and close signal

--- app/device.py
+++ app/device.py
@@ -7,14 +7,16 @@ from app.websocket import websocket_send
 
 class Device:
     door_status = False
-    lock_status = False
+    lock_status = 'closed'
     door_status_detection = False
 
     def __init__(self):
         self.door_status_timer = Timer(1)
         self.door_status_pin = Pin(config.DOOR_STATUS_PIN, Pin.IN)
         self.door_status_pin.irq(self.trigger_door_status)
-        self.lock_pin = Pin(config.LOCK_PIN, Pin.OUT)
+        self.lock_open_pin = Pin(config.LOCK_OPEN_PIN, Pin.OUT)
+        self.lock_close_pin = Pin(config.LOCK_CLOSE_PIN, Pin.OUT)
+        self.door_lock_timer = Timer(2)
 
     def trigger_door_status(self, pin):
         if not self.door_status_detection:
@@ -47,15 +49,30 @@ class Device:
             websocket_send('DoorStatus', 'request', {'status': self.door_status})
 
     def open_lock(self):
-        if self.lock_status:
+        if self.lock_status in ['opening', 'closing']:
             return
-        self.lock_status = True
-        self.lock_pin.value(1)
-        websocket_send('ResourceStatusChange', 'request', {'status': 'open', 'resource_uid': config.RESOURCE_UID})
+        self.lock_status = 'opening'
+        self.lock_open_pin.value(1)
+        self.door_lock_timer.init(mode=Timer.ONE_SHOT, period=250, callback=self.finalize_lock)
+        websocket_send('ResourceStatusChange', 'request', {'status': 'opening', 'resource_uid': config.RESOURCE_UID})
 
     def close_lock(self):
-        if not self.lock_status:
+        if self.lock_status in ['opening', 'closing']:
             return
-        self.lock_status = False
-        self.lock_pin.value(0)
+        self.lock_status = 'closing'
+        self.lock_close_pin.value(1)
+        self.door_lock_timer.init(mode=Timer.ONE_SHOT, period=250, callback=self.finalize_lock)
         websocket_send('ResourceStatusChange', 'request', {'status': 'closed', 'resource_uid': config.RESOURCE_UID})
+
+    def finalize_lock(self):
+        self.lock_open_pin.value(0)
+        self.lock_close_pin.value(0)
+        if self.lock_status == 'opening':
+            self.lock_status = 'open'
+        elif self.lock_status == 'closing':
+            self.lock_status = 'closed'
+        websocket_send(
+            'ResourceStatusChange',
+            'request',
+            {'status': self.lock_status, 'resource_uid': config.RESOURCE_UID}
+        )
